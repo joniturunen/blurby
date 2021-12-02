9dd81a4d96beb762101ca2f261c015b06bd199eb
Joni Turunen <joni.turunen@triplan.fi>
2021-12-02 16:37:46 +0200

    task: Implement a few page function Add HTML5up HTML5 theme for the project

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'flask-test say Hello'
+    return render_template('main.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
+@app.route('/list')
+def list():
+    return render_template('list.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
