d4a607f3360b994881f252c670abbab3c49e67a6
Joni Turunen <joni.turunen@triplan.fi>
2021-12-02 19:39:01 +0200

    task: Initial SQLAlchemy Config

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime as dt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data/sqlite.db'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    blob = db.Column(db.String(256), nullable=False)
+    time_stamp = db.Column(db.DateTime, default=dt.now)
+
+    def __repr__(self):
+        return '<Data %r>' % self.id
+
 
 @app.route('/')
 def index():
@@ -13,6 +26,7 @@ def post():
 @app.route('/list')
 def list():
     return render_template('list.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
